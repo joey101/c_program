5ecdddecaeb38664085b2dc21b1557a86a132cdb
joey101 <jawad.tanana@gmail.com>
2020-12-17 10:11:17 +1100

    week 2 work

---
+++ Week 2/addition.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main () {
+    int a, b, sum;
+    printf("Please enter two integers: ");
+    scanf("%d %d", &a, &b);
+
+    sum = a + b;
+    printf("%d + %d = %d", a, b, sum);
+
+
+}
---
+++ Week 2/negative.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+int main () {
+    int number;
+    scanf("%d", &number);
+    
+    if (number >= 0) {
+
+    printf("You have entered a positive number.\n");
+
+    } else {
+
+    printf("Don't be so negative!\n");
+
+    }
+
+}
