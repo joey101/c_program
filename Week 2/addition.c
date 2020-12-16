716a4100a29ad2f76925cd42d864587f6855b01d
joey101 <jawad.tanana@gmail.com>
2020-12-17 10:16:42 +1100

    adding decriptions
    The purpose of this program is to add two numbers

--- Week 2/addition.c
+++ Week 2/addition.c
@@ -1,9 +1,11 @@
+/* The purpose of this program is to add two numbers */
 #include <stdio.h>
 
 int main () {
-    int a, b, sum;
+    int a, b, sum; // This initiates the variables I am going to use.
     printf("Please enter two integers: ");
-    scanf("%d %d", &a, &b);
+    scanf("%d %d", &a, &b); // This commad allows me to input numbers into the
+                            // terminal.
 
     sum = a + b;
     printf("%d + %d = %d", a, b, sum);
