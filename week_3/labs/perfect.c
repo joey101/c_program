f6d33e013f8e7dddd510e8b9b1b6b8da1d7640aa
Blade Runner <bladerunner@localhost.localdomain>
2021-03-08 22:28:40 +1100

    new file

---
+++ week_3/labs/perfect.c
@@ -0,0 +1,12 @@
+// Reads a positive integer n from standard input and prints all the factors 
+// of n, their sum and indicates whether n is a perfect number.
+// By Jawad Tanana (z5320158)
+
+#include <stdio.h>
+
+int main(void) {
+
+
+}
+
+
