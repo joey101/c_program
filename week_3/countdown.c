397eff4d70d0ed1eb29ec3dc8143b63c9543cf7c
joey101 <jawad.tanana@gmail.com>
2021-03-08 17:50:16 +1100

    idk

--- week_3/countdown.c
+++ week_3/countdown.c
@@ -1,11 +1,13 @@
 // A simple C program that counts down
-// by Jawad Tanana (z5320159@ad.unsw.edu.au0
+// by Jawad Tanana (z5320159@ad.unsw.edu.au)
 
 #include <stdio.h>
 
 
 int main (void) { 
     printf("Fuck me dadddyyyy");
+
+    printf("I am funny");
 
 
 }
