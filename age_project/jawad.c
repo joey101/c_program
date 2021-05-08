35dca4c996e30c0616d34591a729fa9ac8afc6c4
joey101 <jawad.tanana@gmail.com>
2021-05-08 12:21:20 +1000

    yoyo

---
+++ age_project/jawad.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+int main (void) {
+    int age;
+    printf("what is your age?");
+    
+    
+    scanf(" %d", &age);
+    printf("You are %d old", age);
+    
+
+    printf("Jawad");
+}
