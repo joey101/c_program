511c73c98d79b4ab9ad59fe1df1fd65d549fc9b6
joey101 <jawad.tanana@gmail.com>
2021-06-01 19:30:40 +1000

    checking

---
+++ comp1521/test.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int  a;
+char b[100];
+
+int fun1() {
+    int c, d;
+    //...
+}
+
+double e;
+
+int fun2() {
+    int f;
+    static int ff;
+    //...
+    fun1();
+  //  ...
+}
+
+int g;
+
+int main(void) {
+    char h[10];
+    int i;
+//    ...
+    fun2();
+//    ...
+        
+}
+
+
